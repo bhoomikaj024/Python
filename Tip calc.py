b1da0232fdcb72c47a3baf6fa2ad52c41e6ba4d5
bhoomikaj024 <bhoomikaj024@gmail.com>
2025-03-20 17:15:47 +0530

    Delete Tip calc.py

--- Tip calc.py
+++
@@ -1,9 +0,0 @@
-print("Welcomme to the Tip Calculater!")
-bill = float(input("What was the total bill? : Rs."))
-tip_perc = float(input("What percentage of the tip would you like to give? : "))
-persons  = int(input("How many people to split the bill? : "))
-tip = bill * (tip_perc/100)
-total_amt = bill + tip
-share = round(total_amt / persons, 2)
-
-print(f"Each person should pay: Rs.{share}")
