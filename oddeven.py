d6cbb31e99fc761dc764b16557defa2c29653bae
bhoomikaj024 <bhoomikaj024@gmail.com>
2025-03-20 17:15:27 +0530

    Delete oddeven.py

--- oddeven.py
+++
@@ -1,6 +0,0 @@
-#program fo odd number or even finder:
-num = int(input("Enter the number you want to check whether it is ODD or EVEN:\n"))
-if (num % 2 == 0):
-    print("Given number is EVEN.")
-else:
-    print("Given number is ODD.")
